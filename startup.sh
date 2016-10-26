2da81b2f8e5dd13526a0cdf288ab7e420eddd4d4
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-26 12:26:05 +0100

    revised startup script logs/console...

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ vagrant plugin install vagrant-gatling-rsync
 vagrant plugin install vagrant-docker-compose
 
 vagrant up 2>&1 | tee logs/console_log.txt
-  vagrantport=$( grep -ic "Vagrant cannot forward the specified ports on this VM" console_log.txt )
+  vagrantport=$( grep -ic "Vagrant cannot forward the specified ports on this VM" logs/console_log.txt )
   if [ $vagrantport -eq 1 ]; then
     echo "Please open 'VirtualBox' and stop any server's that aren't in use. (Tip: CMD + F)"
     echo "Once done please type 'yes' to continue."
